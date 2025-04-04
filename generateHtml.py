ca367587e32bb0df649adb3687014dc5a5ffcb6f
Kancer <ezeroglukancer@gmail.com>
2025-04-04 11:26:08 +0200

    fix: flask was running constantly, github actions was running non stop. To fix it we created new function and call it in the workflow

---
+++ generateHtml.py
@@ -0,0 +1,3 @@
+from readCsv import generate_static_html
+
+generate_static_html()
--- readCsv.py
+++ readCsv.py
@@ -3,8 +3,7 @@ import csv
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+def generate_static_html():
     with open("supermarket_prices.csv", newline='', encoding="utf-8") as f:
         reader = csv.DictReader(f)
         prices = list(reader)
@@ -15,7 +14,16 @@ def index():
     with open("docs/index.html", "w", encoding="utf-8") as static_file:
         static_file.write(rendered_html)
 
-    return rendered_html
+    print("✅ Static HTML generated at docs/index.html")
+
+@app.route("/")
+def index():
+    with open("supermarket_prices.csv", newline='', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        prices = list(reader)
+
+    return render_template("index.html", prices=prices)
 
 if __name__ == "__main__":
+    # Local development only
     app.run(debug=True)
